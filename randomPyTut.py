2ac48942a43e18b2cfed3bc711dfc315b484e2df
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-01 17:15:59 +0530

    randint()

--- randomPyTut.py
+++ randomPyTut.py
@@ -1,8 +1,4 @@
 import random
 
-for k in range(1,4):
-	random.seed(0)
-	#can be anything, i had it 0
-
-	print(random.randint(5,55))
-	#The first generated value is SEED. It will be the number till last
+random.randint(1,55)
+#Returns an integer that can be 1,55 or between
