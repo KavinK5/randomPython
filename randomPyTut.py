303a42cdd7b7e0fabd44d2ed18c63aa452b7f718
Kavin <140937467+KavinK5@users.noreply.github.com>
2023-12-01 17:17:27 +0530

    choices()

--- randomPyTut.py
+++ randomPyTut.py
@@ -1,10 +1,8 @@
 import random
 
-random.randrange(100)
-#0 to 100
-
-random.randrange(22,220)
-#Returns an integer that can be 22,220 or between
-
-random.randrange(0,100,10)
-#Returns any value in this [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+random.choices(List)
+random.choices(String)
+random.choices(Tuple)
+ 
+#Not for SETS
+#One element will be randomly generated from each
